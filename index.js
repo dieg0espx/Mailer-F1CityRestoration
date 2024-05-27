9b97814cd5ff056ae8ed3cf7f2d81ad6bd8ebf73
dieg0espx <espinosa9mx@gmail.com>
2024-05-27 13:50:11 -0700

    .

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const apiURL = 'https://api.f1cityrestoration.com'
 
 function generateRandomId() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -29,12 +33,42 @@ const data = [
     }
 ];
 
-// Route to handle tracking requests
-app.get('/image/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(`Email with ID ${id} has been opened.`);
-    res.sendFile(path.join(__dirname, 'images/pixel.png')); // Adjust the path to your tracking pixel image
+app.get('/image/:email', (req, res) => {
+    const email = req.params.email;
+    const campaign = req.query.campaign;
+  
+    console.log(`Email ${email} at campaign ${campaign} has been opened.`);
+
+    try {
+        const data = {
+          email: email,
+          timestamp: new Date(),
+          campaign: campaign,
+          action: 'opened'
+        };
+    
+        fetch(apiURL + '/emailOpened.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({ data: data })
+        });
+    
+    } catch (error) {
+        console.error('Error calling external API:', error);
+        res.status(500).json({ status: 500, error: 'Failed to call the external endpoint' });
+    }
+
+
+
+
+    res.sendFile(path.join(__dirname, 'images/pixel.png'));
 });
+  
+
+
 
 app.post('/sendEmail', async (req, res) => {
     try {
@@ -63,11 +97,11 @@ app.post('/sendEmail', async (req, res) => {
                 to: data[i].email,
                 subject: 'Email Attempt',
                 template: 'inventoryNotification',
-                context: {
+                context: { 
                     name: data[i].name,
                     lastName: data[i].lastName,
                     id: data[i].id,
-                    imgURL: `https://mailer-f1-city-restoration.vercel.app/image/${data[i].id}`
+                    imgURL: `https://mailer-f1-city-restoration.vercel.app/image/${data[i].email}?campaign=Campaign1`
                 }
             };
 
@@ -85,6 +119,37 @@ app.post('/sendEmail', async (req, res) => {
         res.status(500).send('Error sending emails');
     }
 });
+
+
+
+app.get('/newOpened', async (req, res) => {
+    try {
+      const data = {
+        email: 'example@example.com',
+        timestamp: '2024-05-30T12:00:00Z',
+        campaign: 'sample_campaign',
+        action: 'opened'
+      };
+  
+      const response = await fetch(apiURL + '/emailOpened.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({ data: data })
+      });
+  
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+  
+      res.status(200).json({ status: 200, message: 'Successfully called the external endpoint' });
+    } catch (error) {
+      console.error('Error calling external API:', error);
+      res.status(500).json({ status: 500, error: 'Failed to call the external endpoint' });
+    }
+  });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
