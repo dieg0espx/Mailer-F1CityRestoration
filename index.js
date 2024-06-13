20a92d2a6df3705c6b0c46e974053c8e40a3ee59
dieg0espx <espinosa9mx@gmail.com>
2024-06-12 19:39:45 -0700

    .

--- index.js
+++ index.js
@@ -142,13 +142,13 @@ app.post('/sendTest', async (req, res) => {
         const customerMailOptions = {
             from: 'info@f1cityrestoration.com',
             to: 'diego@f1cityrestoration.com', 
-            subject: 'Collaborative Opportunity: Enhancing Service Offerings with F1 City Restoration',
-            template: 'brokers1',
+            subject: 'Exciting News From F1 City Group !',
+            template: 'googleContacts',
             context: { 
                 name: 'Diego',
                 lastName: 'Espinosa',
                 company: "Company Name", 
-                imgURL: `https://mailer-f1-city-restoration.vercel.app/image/diego@f1cityrestoration.com?campaign=Brokers1`, 
+                imgURL: `https://mailer-f1-city-restoration.vercel.app/image/diego@f1cityrestoration.com?campaign=GoogleContacts`, 
             }
         };
         try {
